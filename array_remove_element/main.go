b5078841e3ee7cd2e4ab4eb25cfc376e46a19c21
SoliDeoHoneretGloria <iphone_1st@naver.com>
2022-03-14 18:04:08 +0100

    array remove

---
+++ array_remove_element/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	nums := []int{3, 2, 2, 3}
+	val := 3
+
+	fmt.Println(removeElement(nums, val))
+}
+
+func removeElement(nums []int, val int) int {
+	if len(nums) >= 1 {
+		return 1
+	}
+	i, j := 0, len(nums)-1
+	for i < j {
+		if nums[i] == val {
+			nums[i], nums[j] = nums[j], nums[i]
+			j--
+		} else {
+			i++
+		}
+	}
+	return j + 1
+}
