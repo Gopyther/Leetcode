0bfbaf3aad8302fddd1d6c964f2930557ff8b32b
SoliDeoHoneretGloria <iphone_1st@naver.com>
2022-04-05 15:53:01 +0200

    strStr

--- array_string_implement_str/main.go
+++ array_string_implement_str/main.go
@@ -5,36 +5,60 @@ import (
 )
 
 func main() {
-	haystack, needle := "aaa", "aaa"
+	haystack, needle := "harrypotter", "pot"
 	fmt.Println(strStr(haystack, needle))
 }
 
 func strStr(haystack string, needle string) int {
-	if len(haystack) < len(needle) {
-		return -1
-	}
-	needler := []rune(needle)
-	haystacker := []rune(haystack)
-	for i, v := range haystacker {
-		if v == needler[0] {
-			if len(needler) == 1 {
-				return i
+	h := make(map[string]struct{})
+	h[needle] = struct{}{}
+	fmt.Println(h)
+
+	index := 0
+
+	for index < len(haystack) {
+		if haystack[index] == needle[0] {
+			if len(haystack) < index+len(needle) {
+				return -1
 			}
-			for j := 0; j < len(needler); j++ {
-				if i+len(needler) > len(haystacker) {
-					return -1
-				}
-				if haystacker[i+j] != needler[j] {
-					break
-				} else if j == len(needler)-1 {
-					return i
-				}
+			tempS := haystack[index:(index + len(needle))]
+			if _, ok := h[tempS]; ok {
+				return index
 			}
 		}
 
+		index++
 	}
+
 	return -1
 }
+
+// func strStr(haystack string, needle string) int {
+// 	if len(haystack) < len(needle) {
+// 		return -1
+// 	}
+// 	needler := []rune(needle)
+// 	haystacker := []rune(haystack)
+// 	for i, v := range haystacker {
+// 		if v == needler[0] {
+// 			if len(needler) == 1 {
+// 				return i
+// 			}
+// 			for j := 0; j < len(needler); j++ {
+// 				if i+len(needler) > len(haystacker) {
+// 					return -1
+// 				}
+// 				if haystacker[i+j] != needler[j] {
+// 					break
+// 				} else if j == len(needler)-1 {
+// 					return i
+// 				}
+// 			}
+// 		}
+
+// 	}
+// 	return -1
+// }
 
 // func strStr(haystack string, needle string) int {
 // 	return strings.Index(haystack, needle)
