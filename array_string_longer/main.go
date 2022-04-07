e81cc162303d8db7733420a41a04ec08c44a7045
SoliDeoHoneretGloria <iphone_1st@naver.com>
2022-04-07 17:59:26 +0200

    prefix

--- array_string_longer/main.go
+++ array_string_longer/main.go
@@ -3,61 +3,126 @@ package main
 import "fmt"
 
 func main() {
-	strs := []string{"power", "water", "teller"}
+	strs := []string{"a"}
 	fmt.Println(longestCommonPrefix(strs))
 }
 
 func longestCommonPrefix(strs []string) string {
 	shortest := 201
-	short := ""
-	longest := ""
-	buf := ""
+
 	for _, v := range strs {
 		if len(v) < shortest {
 			shortest = len(v)
-			short = v
-		}
-	}
-	for i := shortest; i >= 0; i-- {
-		length := i
-		right := 0
-
-		for _, v := range strs {
-			temp := v
-			for j := 0; j < len(temp); j++ {
-				if j+length >= len(temp) {
-					break
-				}
-				if temp[j:j+length] == short[0:i] {
-					right++
-					buf = short[0:i]
-				}
-			}
-			if right == len(strs) && len(longest) < len(buf) {
-				longest = buf
-			}
 		}
 	}
 
-	for i := 0; i < shortest; i++ {
-		length := shortest - i
-		left := 0
-		for _, v := range strs {
-			temp := v
-			for j := 0; j < len(temp); j++ {
-				if j+length >= len(temp) {
-					break
-				}
-				if temp[j:j+length] == short[i:shortest] {
-					left++
-					buf = short[i:shortest]
-				}
-				if left == len(strs) && len(longest) < len(buf) {
-					longest = buf
-				}
+	for i := shortest; i > 0; i-- {
+		temp := ""
+		for j, k := range strs {
+			if j == 0 {
+				temp = k[0:i]
+				continue
+			}
+			if temp != k[0:i] {
+				break
+			}
+			if temp == k[0:i] && j == len(strs)-1 {
+				return temp
 			}
 		}
-	}
 
-	return longest
+	}
+	return ""
 }
+
+// func longestCommonPrefix(strs []string) string {
+// 	shortest := 201
+// 	short := ""
+// 	longest := ""
+// 	buf := ""
+// 	for _, v := range strs {
+// 		if len(v) < shortest {
+// 			shortest = len(v)
+// 			short = v
+// 		}
+// 	}
+
+// 	for i := shortest; i >= 0; i-- {
+// 		length := i
+// 		right := 0
+
+// 		for _, v := range strs {
+// 			temp := v
+// 			for j := 0; j < len(temp); j++ {
+// 				if j+length > len(temp) {
+// 					break
+// 				}
+// 				if temp[0:j+length] == short[0:i] {
+// 					right++
+// 					buf = short[0:i]
+// 				}
+// 			}
+// 			if right == len(strs) && len(longest) < len(buf) {
+// 				longest = buf
+// 			}
+
+// 		}
+
+// 	}
+// 	return longest
+// }
+
+// func longestCommonPrefix(strs []string) string {
+// // 	shortest := 201
+// 	short := ""
+// 	longest := ""
+// 	buf := ""
+// 	for _, v := range strs {
+// 		if len(v) < shortest {
+// 			shortest = len(v)
+// 			short = v
+// 		}
+// 	}
+// 	for i := shortest; i >= 0; i-- {
+// 		length := i
+// 		right := 0
+
+// 		for _, v := range strs {
+// 			temp := v
+// 			for j := 0; j < len(temp); j++ {
+// 				if j+length > len(temp) {
+// 					break
+// 				}
+// 				if temp[j:j+length] == short[0:i] {
+// 					right++
+// 					buf = short[0:i]
+// 				}
+// 			}
+// 			if right == len(strs) && len(longest) < len(buf) {
+// 				longest = buf
+// 			}
+// 		}
+// 	}
+
+// 	for i := 0; i < shortest; i++ {
+// 		length := shortest - i
+// 		left := 0
+// 		for _, v := range strs {
+// 			temp := v
+// 			for j := 0; j < len(temp); j++ {
+// 				if j+length > len(temp) {
+// 					break
+// 				}
+// 				if temp[j:j+length] == short[i:shortest] {
+// 					left++
+// 					buf = short[i:shortest]
+// 				}
+// 				if left == len(strs) && len(longest) < len(buf) {
+// 					longest = buf
+// 				}
+// 			}
+// 		}
+// 	}
+
+// 	return longest
+// }
