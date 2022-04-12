6ffb3a84cb5ab1f0670ea5e18401ad7180ca2e73
SoliDeoHoneretGloria <iphone_1st@naver.com>
2022-04-12 18:22:23 +0200

    update

--- array_string_subarray/main.go
+++ array_string_subarray/main.go
@@ -12,15 +12,27 @@ func main() {
 }
 
 func minSubArrayLen(target int, nums []int) int {
-	lp, rp := 0, 0
+	lp, rp := -1, -1
 	sum := 0
 	size := math.MaxInt
 
 	for rp < len(nums) || lp < len(nums) {
-		if sum < target {
-
+		if sum < target && rp != len(nums)-1 {
+			rp++
+			sum += nums[rp]
+		} else if sum >= target && lp < rp {
+			lp++
+			sum -= nums[lp]
+		} else {
+			break
+		}
+		if sum == target {
+			fmt.Println(rp, lp)
+			if size > 1+rp-lp {
+				size = 1 + rp - lp
+			}
 		}
 
 	}
-	return 0
+	return size
 }
